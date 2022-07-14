aeb9ec8019c9b39d3e2cc8b741a619ce83afc38c
Loacking <LudovicDop@gmail.com>
2022-07-14 17:57:54 +0200

    probleme de commande de compilation ok

--- appTest/appTest/main.c
+++ appTest/appTest/main.c
@@ -3,15 +3,37 @@
 //  appTest
 //
 //  Created by Ludovic Doppler on 12/07/2022.
-//
+
+//gcc main.c -o prog -F/Library/Frameworks -framework SDL2 (pour la compil)
+//./prog pour executer le code
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
-int main(int argc, const char * argv[]) {
-    SDL_version nb;
-    SDL_VERSION(&nb);
+int main(int argc,char * argv[]) {
     
-    printf("Bienvenue sur la SDL %d,%d,%d !\n",nb.major,nb.minor,nb.patch);
+    SDL_Window *window = NULL;
     
-    return 0;
+    
+    if( SDL_Init(SDL_INIT_VIDEO) != 0 ){
+        SDL_Log("Error : Initialisation SDL > %s\n",SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    //Exe du prog...
+    window = SDL_CreateWindow("First window SDL2",
+                              SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,
+                              SDL_WINDOW_MINIMIZED);
+    
+    if(window == NULL){
+        SDL_Log("Error : Initialisation SDL > %s\n",SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    SDL_Delay(5000);
+    
+    
+    
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
 }
