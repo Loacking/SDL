aad0118654daf8d35c47d1a20bb3051e4bd2d866
Loacking <LudovicDop@gmail.com>
2022-07-16 12:01:48 +0200

    sdl suite

--- appTest/appTest/main.c
+++ appTest/appTest/main.c
@@ -14,26 +14,33 @@ void SDL_ExitWithError(const char *message);
 int main(int argc,char * argv[]) {
     
     SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
     
     //Lancment SDL
     if( SDL_Init(SDL_INIT_VIDEO) != 0 ){
         SDL_ExitWithError("Initialisation SDL");
     }
-    
-    //Création de la fenêtre
-    window = SDL_CreateWindow("First window SDL2",
-                              SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,
-                              SDL_WINDOW_MINIMIZED);
-    
-    if(window == NULL){
-        SDL_ExitWithError("Création fenetre echouee");
+    //Création fenetre + rendu
+    if(SDL_CreateWindowAndRenderer(800,600, SDL_WINDOW_MINIMIZED, &window,&renderer) != 0){
+        SDL_ExitWithError("Impossible de creer la fenetre et le rendu");
+    }
+
+    /*  ---------------- */
+       
+       if(SDL_SetRenderDrawColor(renderer,112,168,237, SDL_ALPHA_OPAQUE) != 0){
+        SDL_ExitWithError("Erreur de chargement de la couleur");
     }
     
-    /*  ---------------- */
-    SDL_Delay(6000);
+       
+       if(SDL_RenderDrawPoint(renderer, 50, 150) != 0){
+           SDL_ExitWithError("Impossible de dessiner un point");
+    }
+       
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
     /*  ---------------- */
     
-    
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     
